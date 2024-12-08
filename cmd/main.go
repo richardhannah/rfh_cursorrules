f006491f3269bc09f2696abce9c985966fd8159e
richard hannah <richard.hannah1974@gmail.com>
2024-12-08 21:56:45 +0000

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	fmt.Print("hello world")
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
