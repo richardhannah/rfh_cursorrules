ae984116093caf36648b514cc72a65801a2aa452
richard hannah <richard.hannah1974@gmail.com>
2025-06-02 21:38:00 +0100

    add tests

--- internal/db/DbContext_test.go
+++ internal/db/DbContext_test.go
@@ -1,10 +1,12 @@
 package db
 
 import (
+	"database/sql"
 	"fmt"
 	sq "github.com/Masterminds/squirrel"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 	"totmapi/internal/config"
 	"totmapi/internal/models"
 )
@@ -61,4 +63,114 @@ func TestSelectUsersWithPredicate(t *testing.T) {
 		t.Fatal(err)
 	}
 	fmt.Printf("Loaded %d users\n", len(users))
+}
+
+func TestInsert(t *testing.T) {
+
+	testSubject := newTestContext(t)
+
+	newPost := models.Blogposts{
+		BlogpostID: "some-uuid",
+		Title:      sql.NullString{String: "New Title", Valid: true},
+		Markdown:   sql.NullString{String: "Markdown body", Valid: true},
+		Category:   sql.NullString{String: "test", Valid: true},
+		Image:      sql.NullString{Valid: false}, // will insert NULL
+		Video:      sql.NullString{String: "xyz123", Valid: true},
+		Date:       sql.NullTime{Time: time.Now(), Valid: true},
+		Published:  false,
+	}
+
+	newPosts := make([]models.Blogposts, 0)
+	newPosts = append(newPosts, newPost)
+
+	predicate := func(ib sq.InsertBuilder) sq.InsertBuilder {
+		return ib.
+			Columns(
+				"blogpostid",
+				"title",
+				"markdown",
+				"category",
+				"image",
+				"video",
+				"date",
+				"published",
+			).
+			Values(
+				newPost.BlogpostID,
+				newPost.Title,
+				newPost.Markdown,
+				newPost.Category,
+				newPost.Image,
+				newPost.Video,
+				newPost.Date,
+				newPost.Published,
+			)
+	}
+
+	err := testSubject.Insert(&newPosts, predicate)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func TestUpdate(t *testing.T) {
+
+	testSubject := newTestContext(t)
+
+	newPost := models.Blogposts{
+		BlogpostID: "some-uuid",
+		Title:      sql.NullString{String: "New Title", Valid: true},
+		Markdown:   sql.NullString{String: "Markdown body", Valid: true},
+		Category:   sql.NullString{String: "test", Valid: true},
+		Image:      sql.NullString{Valid: false}, // will insert NULL
+		Video:      sql.NullString{String: "xyz123", Valid: true},
+		Date:       sql.NullTime{Time: time.Now(), Valid: true},
+		Published:  false,
+	}
+
+	newPosts := make([]models.Blogposts, 0)
+	newPosts = append(newPosts, newPost)
+
+	predicate := func(ub sq.UpdateBuilder) sq.UpdateBuilder {
+		return ub.
+			Set("title", "updated Title").
+			Where(
+				sq.Eq{"blogpostid": "some-uuid"},
+			)
+	}
+
+	err := testSubject.Update(&newPosts, predicate)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func TestDelete(t *testing.T) {
+
+	testSubject := newTestContext(t)
+
+	newPost := models.Blogposts{
+		BlogpostID: "some-uuid",
+		Title:      sql.NullString{String: "New Title", Valid: true},
+		Markdown:   sql.NullString{String: "Markdown body", Valid: true},
+		Category:   sql.NullString{String: "test", Valid: true},
+		Image:      sql.NullString{Valid: false}, // will insert NULL
+		Video:      sql.NullString{String: "xyz123", Valid: true},
+		Date:       sql.NullTime{Time: time.Now(), Valid: true},
+		Published:  false,
+	}
+
+	newPosts := make([]models.Blogposts, 0)
+	newPosts = append(newPosts, newPost)
+
+	predicate := func(db sq.DeleteBuilder) sq.DeleteBuilder {
+		return db.Where(
+			sq.Eq{"blogpostid": "some-uuid"},
+		)
+	}
+
+	err := testSubject.Delete(&newPosts, predicate)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
